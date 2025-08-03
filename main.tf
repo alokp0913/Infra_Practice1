3271fe43dcb71c7bbd745ba71df144de4309aecc
Alok Vaishya <alokp0913@gmail.com>
2025-08-03 20:13:47 +0530

    1 Rg and STG added

--- main.tf
+++ main.tf
@@ -12,3 +12,20 @@ resource "azurerm_storage_account" "stg65" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+
+
+resource "azurerm_resource_group" "uuis7" {
+  name     = "alok-rg2134"
+  location = "east us"
+}
+
+
+resource "azurerm_storage_account" "stg65" {
+    depends_on = [ azurerm_resource_group.ok223 ]
+  name                     = "alokssa7552"
+  resource_group_name      = "alok-rg234"
+  location                 = "east us"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
